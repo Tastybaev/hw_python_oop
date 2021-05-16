2c3b73769059cc689d53696db427e79c1e8d25ce
Аскар <Tastybaev@mail.ru>
2021-05-16 21:34:38 +0500

    bug fix 2

--- homework.py
+++ homework.py
@@ -5,47 +5,46 @@ from typing import Optional
 class Record:
     def __init__(self, amount: float, comment: str, \
         date: Optional[str] = None) -> None:
-        self.amount = amount
-        self.comment = comment
-        if date is None:
-            self.date = dt.datetime.today().date()
-        else:
-            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+            self.amount = amount
+            self.comment = comment
+            if date is None:
+                self.date = dt.datetime.today().date()
+            else:
+                self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
 
 
 class Calculator:
     def add_record(self, record: Record):
-       self.records.append(record)
-    
-    
+        self.records.append(record)
+
+
     def __init__(self, limit):
         self.limit = limit
         self.records= []
-    
-    
+
+
     def get_today_stats(self):
-        return sum(i.amount for i in self.records if i.date == dt.datetime.today().date())
-    
-    
+        return sum(i.amount for i in self.records \
+            if i.date == dt.datetime.today().date())
+
+
     def get_week_stats(self):
         day_week_ago = dt.datetime.today().date() - dt.timedelta(days=7)
         return sum(i.amount for i in self.records if i.date >= day_week_ago)
 
 
 class CaloriesCalculator(Calculator):
-    
-    
     def get_calories_remained(self, limit):
         calories_limit = self.get_today_stats()
         if calories_limit <= limit:
             over_limit = limit - calories_limit
-            return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {over_limit} кКал'
+            return f'Сегодня можно съесть что-нибудь ещё, \
+                но с общей калорийностью не более {over_limit} кКал'
         else:
             return 'Хватит есть!'
 
 
 class CashCalculator(Calculator):
-
     EXCHANGE_RATE = {
         'rub': 1,
         'usd': 73.99,
@@ -76,6 +75,7 @@ class CashCalculator(Calculator):
             debt = abs(round((self.get_today_stats() - self.limit)/rate, 2))
             return f'Денег нет, держись: твой долг - {debt} {cur_name}'
 
+
 # создадим калькулятор денег с дневным лимитом 1000
 cash_calculator = CashCalculator(1000)
 
@@ -93,3 +93,4 @@ cash_calculator.add_record(Record(amount=3000,
 print(cash_calculator.get_today_cash_remained('rub'))
 # должно напечататься
 # На сегодня осталось 555 руб 
+ 
