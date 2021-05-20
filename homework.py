285e33f019adf6a3b6c1291a8fd7aee6de307d06
Аскар <Tastybaev@mail.ru>
2021-05-20 23:33:51 +0500

    3/4

--- homework.py
+++ homework.py
@@ -1,14 +1,18 @@
 import datetime as dt
 from typing import Optional
+
+
 class Record:
     def __init__(self, amount: float, comment: str,
                  date: Optional[str] = None) -> None:
         self.amount = amount
         self.comment = comment
         if date is None:
-            self.date = dt.date.today() 
+            self.date = dt.date.today()
         else:
             self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+
+
 class Calculator:
     def add_record(self, record: Record):
         self.records.append(record)
@@ -17,13 +21,15 @@ class Calculator:
         self.records = []
     def get_today_stats(self):
         return sum(i.amount for i in self.records
-                   if i.date == dt.date.today() )
+                   if i.date == dt.date.today())
     def get_week_stats(self):
         today = dt.date.today().date()
         tomorrow = today + dt.timedelta(days=1)
         week_ago = today - dt.timedelta(days=7)
-        return sum(i.amount for i in self.records if i.date >= week_ago 
+        return sum(i.amount for i in self.records if i.date >= week_ago
                    and i.date < tomorrow)
+
+
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self, limit):
         calories_limit = self.get_today_stats()
@@ -33,9 +39,12 @@ class CaloriesCalculator(Calculator):
                 но с общей калорийностью не более {over_limit} кКал'
         else:
             return 'Хватит есть!'
+
+
 class CashCalculator(Calculator):
     USD_RATE = 73.99
     EURO_RATE = 89.67
+
     def get_today_cash_remained(self, currency):
         self.currency = currency
         money_type = ''
@@ -63,6 +72,8 @@ class CashCalculator(Calculator):
             return 'Денег нет, держись'
         else:
             return f'Денег нет, держись: твой долг - {debt} {money_type}'
+
+
 # создадим калькулятор денег с дневным лимитом 1000
 cash_calculator = CashCalculator(1000)
 # дата в параметрах не указана,
