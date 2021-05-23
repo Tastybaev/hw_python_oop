d7cc2b168c7c6129851141a25f8f735095d347cd
Аскар <Tastybaev@mail.ru>
2021-05-23 19:08:54 +0500

    bug fix 3/4

--- homework.py
+++ homework.py
@@ -45,35 +45,36 @@ class CaloriesCalculator(Calculator):
 
 
 class CashCalculator(Calculator):
-    EXCHANGE_RATE = {
-        'rub': 1,
-        'usd': 73.99,
-        'eur': 89.67,
-    }
-
-    CURRENCY_NAME = {
-        'rub': 'руб',
-        'usd': 'USD',
-        'eur': 'Euro',
-    }
+    USD_RATE = 73.99
+    EURO_RATE = 89.67
 
     def get_today_cash_remained(self, currency):
-        cur = currency.lower()
-        cur_name = CashCalculator.CURRENCY_NAME[cur]
-        rate = CashCalculator.EXCHANGE_RATE[cur]
-
-        cash_remained = self.limit - round(self.get_today_stats(), 2)
-        if rate != 1.0:
-            cash_remained = round(cash_remained / rate, 2)
-
-        if round(self.get_today_stats(), 2) < self.limit:
-            return f'На сегодня осталось {cash_remained} {cur_name}'
-        elif round(self.get_today_stats(), 2) == self.limit:
+        self.currency = currency
+        money_type = ''
+        if currency.lower() == 'rub':
+            money_type = 'руб'
+            cash_remained = round(self.limit - self.get_today_stats(), 2)
+            debt = abs(round(self.get_today_stats() - self.limit, 2))
+        elif currency.lower() == 'usd':
+            money_type = 'USD'
+            cash_remained = round((self.limit - self.get_today_stats())
+                                  / self.USD_RATE, 2)
+            debt = abs(round((self.get_today_stats() - self.limit)
+                             / self.USD_RATE, 2))
+        elif currency.lower() == 'eur':
+            money_type = 'Euro'
+            cash_remained = round((self.limit - self.get_today_stats())
+                                  / self.EURO_RATE, 2)
+            debt = abs(round((self.get_today_stats() - self.limit)
+                             / self.EURO_RATE, 2))
+        if self.get_today_stats() == 0:
+            return 'Денег нет, держись'
+        if self.get_today_stats() < self.limit:
+            return f'На сегодня осталось {cash_remained} {money_type}'
+        elif self.get_today_stats() == self.limit:
             return 'Денег нет, держись'
         else:
-            debt = abs(round((self.get_today_stats(), 2)) - self.limit
-                            / abs(round(rate, 2)))
-            return f'Денег нет, держись: твой долг - {debt} {cur_name}'
+            return f'Денег нет, держись: твой долг - {debt} {money_type}'
 
 
 # создадим калькулятор денег с дневным лимитом 1000
@@ -95,4 +96,4 @@ caloriescalculator = CaloriesCalculator(1000)
 caloriescalculator.add_record(Record(amount=1186,
             comment='Кусок тортика. И ещё один.',
             date='24.02.2019'))
-print(caloriescalculator.get_calories_remained(9))
+print(caloriescalculator.get_calories_remained(2000))
