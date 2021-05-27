d4efa5b163b87d7391255c5b229448de6bcadf1c
Аскар <Tastybaev@mail.ru>
2021-05-27 23:15:08 +0500

    final version 2

--- homework.py
+++ homework.py
@@ -22,24 +22,27 @@ class Calculator:
         self.records = []
 
     def get_today_stats(self):
+        today = dt.date.today()
         return sum(i.amount for i in self.records
-                   if i.date == dt.date.today())
+                   if i.date == today)
 
     def get_week_stats(self):
         today = dt.date.today()
-        tomorrow = today + dt.timedelta(days=1)
         week_ago = today - dt.timedelta(days=7)
-        return sum(i.amount for i in self.records 
-                   if tomorrow > i.date >= week_ago)
+        return sum(i.amount for i in self.records
+                   if today >= i.date >= week_ago)
+
+    def get_balance(self):
+        return self.limit - self.get_today_stats()
 
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
         calories_limit = self.get_today_stats()
         if calories_limit <= self.limit:
-            over_limit = self.limit - self.get_today_stats()
-            return (('Сегодня можно съесть что-нибудь ещё, '),
-                   (f'но с общей калорийностью не более {over_limit} кКал'))
+            over_limit = self.get_balance()
+            return ''.join(['Сегодня можно съесть что-нибудь ещё, ',
+                   f'но с общей калорийностью не более {over_limit} кКал'])
         return 'Хватит есть!'
 
 
@@ -48,40 +51,22 @@ class CashCalculator(Calculator):
     USD_RATE = 73.99
     EURO_RATE = 89.67
 
-    currency_info = {'rub': (RUB_RATE, 'руб',),
-                     'usd' : (USD_RATE, 'USD'),
-                     'eur': (EURO_RATE, 'Euro')
-                    }
-
-
     def get_today_cash_remained(self, currency):
-        rate, title = self.currency_info[currency]
-        cash_remained = round(self.limit - self.get_today_stats(), 2)
-        if self.get_today_stats() == self.limit:
+        currency_info = {
+            'rub': (self.RUB_RATE, 'руб',),
+            'usd' : (self.USD_RATE, 'USD'),
+            'eur': (self.EURO_RATE, 'Euro')
+        }
+
+        cash_remained = self.get_balance()
+        if cash_remained == 0:
             return 'Денег нет, держись'
-        elif rate != 1.0:
+
+        rate, title = currency_info[currency.lower()]
+        if rate != 1.0:
             cash_remained = round(cash_remained / rate, 2)
-       
-        if self.get_today_stats() < self.limit:
+
+        if cash_remained > 0:
             return f'На сегодня осталось {cash_remained} {title}'
-        
-        if cash_remained < 0:
-            debt = abs(round((self.get_today_stats() - self.limit) / rate, 2))
-            return f'Денег нет, держись: твой долг - {debt} {title}'
-
-
-# создадим калькулятор денег с дневным лимитом 1000
-cash_calculator = CashCalculator(1000)
-# дата в параметрах не указана,
-# так что по умолчанию к записи
-# должна автоматически добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=145, comment='кофе'))
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000,
-                                  comment='бар в Танин др',
-                                  date='08.11.2019'))
-print(cash_calculator.get_today_cash_remained('rub'))
-# должно напечататься
-# На сегодня осталось 555 руб
+        debt = abs(cash_remained)
+        return f'Денег нет, держись: твой долг - {debt} {title}'
