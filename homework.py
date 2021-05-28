6cf45727d60492fbd79ec9d27129798e6c40757e
Аскар <Tastybaev@mail.ru>
2021-05-28 13:25:21 +0500

    final version 2

--- homework.py
+++ homework.py
@@ -22,74 +22,51 @@ class Calculator:
         self.records = []
 
     def get_today_stats(self):
+        today = dt.date.today()
         return sum(i.amount for i in self.records
-                   if i.date == dt.date.today())
+                   if i.date == today)
 
     def get_week_stats(self):
         today = dt.date.today()
-        tomorrow = today + dt.timedelta(days=1)
         week_ago = today - dt.timedelta(days=7)
-        return sum(i.amount for i in self.records if i.date >= week_ago
-                   and i.date < tomorrow)
+        return sum(i.amount for i in self.records
+                   if today >= i.date >= week_ago)
+
+    def get_balance(self):
+        return self.limit - self.get_today_stats()
 
 
 class CaloriesCalculator(Calculator):
-    def __init__(self, limit):
-        super().__init__(limit)
-
     def get_calories_remained(self):
         calories_limit = self.get_today_stats()
         if calories_limit <= self.limit:
-            over_limit = self.limit - calories_limit
-            return 'Сегодня можно съесть что-нибудь ещё, ' + \
-                   f'но с общей калорийностью не более {over_limit} кКал'
-        else:
-            return 'Хватит есть!'
+            over_limit = self.get_balance()
+            return ''.join(['Сегодня можно съесть что-нибудь ещё, ',
+                   f'но с общей калорийностью не более {over_limit} кКал'])
+        return 'Хватит есть!'
 
 
 class CashCalculator(Calculator):
+    RUB_RATE = 1
     USD_RATE = 73.99
     EURO_RATE = 89.67
 
     def get_today_cash_remained(self, currency):
-        self.currency = currency
-        money_type = ''
-        if currency.lower() == 'rub':
-            money_type = 'руб'
-            cash_remained = self.limit - self.get_today_stats()
-            debt = self.get_today_stats() - self.limit
-        elif currency.lower() == 'usd':
-            money_type = 'USD'
-            cash_remained = round((self.limit - self.get_today_stats())
-                                  / self.USD_RATE, 2)
-            debt = abs(round((self.get_today_stats() - self.limit)
-                             / self.USD_RATE, 2))
-        elif currency.lower() == 'eur':
-            money_type = 'Euro'
-            cash_remained = round((self.limit - self.get_today_stats())
-                                  / self.EURO_RATE, 2)
-            debt = abs(round((self.get_today_stats() - self.limit)
-                             / self.EURO_RATE, 2))
-        if self.get_today_stats() < self.limit:
-            return f'На сегодня осталось {cash_remained} {money_type}'
-        elif self.get_today_stats() == self.limit:
+        currency_info = {
+            'rub': (self.RUB_RATE, 'руб',),
+            'usd' : (self.USD_RATE, 'USD'),
+            'eur': (self.EURO_RATE, 'Euro')
+        }
+
+        cash_remained = self.get_balance()
+        if cash_remained == 0:
             return 'Денег нет, держись'
-        else:
-            return f'Денег нет, держись: твой долг - {debt} {money_type}'
 
+        rate, title = currency_info[currency.lower()]
+        if rate != 1.0:
+            cash_remained = round(cash_remained / rate, 2)
 
-# создадим калькулятор денег с дневным лимитом 1000
-cash_calculator = CashCalculator(1000)
-# дата в параметрах не указана,
-# так что по умолчанию к записи
-# должна автоматически добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=145, comment='кофе'))
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000,
-                                  comment='бар в Танин др',
-                                  date='08.11.2019'))
-print(cash_calculator.get_today_cash_remained('rub'))
-# должно напечататься
-# На сегодня осталось 555 руб
+        if cash_remained > 0:
+            return f'На сегодня осталось {cash_remained} {title}'
+        debt = abs(cash_remained)
+        return f'Денег нет, держись: твой долг - {debt} {title}'
